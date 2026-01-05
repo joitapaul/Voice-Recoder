82e522ec31bb7f075e3ee088763659c7b26a8b9c
JOITA PAUL <joitapaul61@gmail.com>
2026-01-05 23:18:49 +0530

    Initial commit - Voice Recorder

---
+++ recorder.py
@@ -0,0 +1,12 @@
+import sounddevice
+from scipy.io.wavfile import write
+
+
+fps =44100
+duration = 10
+print("RECORDING...")
+recording = sounddevice.rec(int(duration*fps), samplerate=fps, channels=2)
+sounddevice.wait()
+print("Done!")
+
+write("Output.wav",fps, recording)
